b4df499c032261500bc7712d2155195b878ee8e8
Андрей Степанов <therain3164@yandex.ru>
2024-09-28 15:14:22 +0300

    task1 2

---
+++ 2week/task1.py
@@ -0,0 +1,43 @@
+"""
+leetcode.com/problem-list/string/
+url: https://leetcode.com/problems/letter-combinations-of-a-phone-number/description
+"""
+
+
+class Solution:
+    def letterCombinations(self, digits: str) -> list[str]:
+        dictionary = {
+            2: "abc",
+            3: "def",
+            4: "ghi",
+            5: "jkl",
+            6: "mno",
+            7: "pqrs",
+            8: "tuv",
+            9: "wxyz",
+        }
+        digits = list(map(int, digits))
+        s = []
+        for digit in digits:
+            s.append(dictionary[digit])
+
+        ans = []
+        n = len(digits)
+        indices = [0] * n
+
+        if len(indices) == 0:
+            return ans
+
+        while True:
+            cur = ""
+            for i in range(len(indices)):
+                cur += s[i][indices[i]]
+            ans.append(cur)
+
+            indices[-1] += 1
+            for i in range(len(indices) - 1, -1, -1):
+                if indices[i] >= len(s[i]):
+                    if i == 0:
+                        return ans
+                    indices[i] = 0
+                    indices[i - 1] += 1
